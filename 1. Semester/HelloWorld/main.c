83a18ea044941d3bc78eac20bb42c9a48525fe59
unknown <thomasalpert@gmail.com>
2020-12-01 23:47:12 +0100

    1

--- 1. Semester/HelloWorld/main.c
+++ 1. Semester/HelloWorld/main.c
@@ -1,8 +1,9 @@
 #include<string.h>
 int main()
 {
-	char abc[] = "Die schoenen Dinge im Leben sind umsonst.";
+	char abc[] = "Die sc1hoenen Dinge im Leben sind umsonst.";
 	int laenge = strlen(abc);
 	printf("La1enge: %d Zeichen\n", laenge);
 	return 0;
+	
 }
