95b90b68d11bbda298d3126c17270983459a76ca
unknown <thomasalpert@gmail.com>
2020-12-01 23:38:28 +0100

    go

---
+++ 1. Semester/Euklidscher_Alogrythmus/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+  int x,y;
+    printf("ZAHL 1 \n ");
+    scanf_s("%i",&x);
+    fflush(stdin);
+    printf("ZAHL 2 \n");
+    scanf("%i",&y);
+    fflush(stdin);
+    while ( x !=y ){
+        if (x > y)
+        {
+            x = x-y;
+        }
+        else
+        {
+            y = y-x;
+        }
+    }
+printf("GGT=    %i ",x);
+
+    return 0;
+}
---
+++ 1. Semester/HelloWorld/main.c
@@ -0,0 +1,8 @@
+#include<string.h>
+int main()
+{
+	char abc[] = "Die schoenen Dinge im Leben sind umsonst.";
+	int laenge = strlen(abc);
+	printf("La1enge: %d Zeichen\n", laenge);
+	return 0;
+}
