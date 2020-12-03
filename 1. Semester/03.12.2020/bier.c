3fe3bb3272807766d1d4b944d22510fa49d1b911
unknown <thomasalpert@gmail.com>
2020-12-03 19:14:52 +0100

    df

--- 1. Semester/03.12.2020/bier.c
+++ 1. Semester/03.12.2020/bier.c
@@ -17,7 +17,7 @@ printf("%f Grundflaeche = ", grundflaeche);
 float volumen = 0;
 for (float i = 0; i < 200; i= i +10 )
 {
-    volumen = ((grundflaeche *i/1000000));
+    volumen = ((grundflaeche *i /1000000));
     printf("\n Das Volumen Betraegt: %f",volumen );
 }
 
