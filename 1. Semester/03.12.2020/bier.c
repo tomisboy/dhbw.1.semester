753ff0bb9117fa66de25dfa3e6f27d31b671ff71
Thomas Alpert <thomasalpert@gmail.com>
2020-12-03 16:16:59 +0100

    1

--- 1. Semester/03.12.2020/bier.c
+++ 1. Semester/03.12.2020/bier.c
@@ -7,5 +7,18 @@
 
 int main() {
 
-    
+float f1 = 123.234;
+float f2 = 12.3234;
+
+
+printf("%f",f2);
+printf("\n%7.2f",f1);
+
+
+printf("\n Fast alle sind flei %c ig",0xe1);
+//printf("\n text=---%s---", text);
+
+
+
+
 }
